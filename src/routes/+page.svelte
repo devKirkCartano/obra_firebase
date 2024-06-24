28da24abfb5713201b306acd020ed4b740536d26
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2024-06-25 02:28:54 +0800

    setup login and forgot

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="center-card">
+  <div class="gradient-outline-text pb-5">OBRA</div>
+  <div class="container d-flex justify-content-center">
+    <div class="card bg-color text-white" style="width: 806px; height: 700px;">
+      <div class="py-5 text-center">
+        <h4 class="gradient-text">LOGIN</h4>
+      </div>
+      <div class="px-5 card-body">
+        <div id="message-login"></div>
+        <form method="POST" id="login-form">
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input
+              type="email"
+              class="form-control input-bg"
+              id="email"
+              name="email"
+            />
+          </div>
+          <div class="form-group py-5">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              class="form-control input-bg"
+              id="password"
+              name="password"
+            />
+          </div>
+          <div class="d-flex justify-content-end py-3">
+            <a href="/forgot">Forgot Password?</a>
+          </div>
+          <div class="d-flex justify-content-center py-5">
+            <button type="submit" class="btn btn-style">LOGIN</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</main>
---
+++ src/routes/forgot/+page.svelte
@@ -0,0 +1,38 @@
+<main class="center-card">
+  <div class="gradient-outline-text pb-5">OBRA</div>
+  <div class="container d-flex justify-content-center">
+    <div class="card bg-color text-white" style="width: 806px; height: 700px;">
+      <div class="py-5 text-center">
+        <h4 class="gradient-text">FORGOT PASSWORD</h4>
+      </div>
+      <div class="px-5 card-body">
+        <div id="message-forgot"></div>
+        <div class="h-100">
+          <form action="/forgot" id="reset-form">
+            <div>
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input
+                  type="email"
+                  class="form-control input-bg"
+                  id="email_reset"
+                  name="email_reset"
+                />
+              </div>
+              <div class="d-flex justify-content-center my-5 py-5">
+                <button type="submit" class="btn btn-style"
+                  >SEND VERIFICATION</button
+                >
+              </div>
+            </div>
+          </form>
+          <div class="d-flex justify-content-center my-5 py-5">
+            <button class="btn btn-dark text-center"
+              ><a href="/">Proceed to Login</a></button
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
