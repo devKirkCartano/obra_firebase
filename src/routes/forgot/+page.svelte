3521ac171e71ac07b1aeda07a53f014f342019e9
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2024-07-06 04:11:50 +0800

    add forgot password reset

--- src/routes/forgot/+page.svelte
+++ src/routes/forgot/+page.svelte
@@ -1,3 +1,43 @@
+<script>
+  // @ts-ignore
+  import { onMount } from "svelte";
+  import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+
+  let email = "";
+  // @ts-ignore
+  let password = "";
+  let message = "";
+  let alertType = "";
+  // @ts-ignore
+  function showMessage(msg, type) {
+    message = msg;
+    alertType = type;
+    setTimeout(() => {
+      message = "";
+      alertType = "";
+    }, 5000); // Hide message after 5 seconds
+  }
+
+  async function handleReset() {
+    const auth = getAuth();
+    try {
+      await sendPasswordResetEmail(auth, email);
+      // Handle success, show message to user
+      showMessage("Password reset email sent successfully", "alert-success");
+
+      console.log("Password reset email sent successfully");
+    } catch (error) {
+      // @ts-ignore
+      const errorMessage = error.message;
+
+      // Handle error, show error message to user
+      showMessage(errorMessage, "alert-danger");
+
+      console.error(error);
+    }
+  }
+</script>
+
 <main class="center-card">
   <div class="gradient-outline-text pb-5">OBRA</div>
   <div class="container d-flex justify-content-center">
@@ -6,9 +46,13 @@
         <h4 class="gradient-text">FORGOT PASSWORD</h4>
       </div>
       <div class="px-5 card-body">
-        <div id="message-forgot"></div>
+        {#if message}
+          <div id="message-login" class={`alert ${alertType}`} role="alert">
+            {message}
+          </div>
+        {/if}
         <div class="h-100">
-          <form action="/forgot" id="reset-form">
+          <form on:submit|preventDefault={handleReset} id="reset-form">
             <div>
               <div class="form-group">
                 <label for="email">Email</label>
@@ -17,6 +61,7 @@
                   class="form-control input-bg"
                   id="email_reset"
                   name="email_reset"
+                  bind:value={email}
                 />
               </div>
               <div class="d-flex justify-content-center my-5 py-5">
@@ -36,3 +81,7 @@
     </div>
   </div>
 </main>
+
+<style>
+  /* Add your custom styles here */
+</style>
